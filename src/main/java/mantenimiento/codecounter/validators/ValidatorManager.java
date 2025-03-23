81fec15ca420b7c7a848e1b1d7622db608e403d8
JoséC <a17000597@alumnos.uady.mx>
2025-03-22 18:46:42 -0600

    refactor: :recycle: implementation of the required verifiers

--- src/main/java/mantenimiento/codecounter/validators/ValidatorManager.java
+++ src/main/java/mantenimiento/codecounter/validators/ValidatorManager.java
@@ -2,15 +2,7 @@ package mantenimiento.codecounter.validators;
 
 import mantenimiento.codecounter.interfaces.FormatValidatorHandler;
 import mantenimiento.codecounter.interfaces.LogicalValidatorHandler;
-import mantenimiento.codecounter.validators.formatValidators.ImportValidator;
-import mantenimiento.codecounter.validators.formatValidators.SingleAnnotationValidator;
-import mantenimiento.codecounter.validators.formatValidators.SingleDeclarationValidator;
-import mantenimiento.codecounter.validators.formatValidators.StyleKAndRValidator;
-import mantenimiento.codecounter.validators.logicalValidators.FlowControlDeclarationValidator;
-import mantenimiento.codecounter.validators.logicalValidators.LambdaDeclarationValidator;
 import mantenimiento.codecounter.validators.logicalValidators.MethodDeclarationValidator;
-import mantenimiento.codecounter.validators.logicalValidators.TernaryDeclarationValidator;
-import mantenimiento.codecounter.validators.logicalValidators.TryDeclarationValidator;
 import mantenimiento.codecounter.validators.logicalValidators.TypeDeclarationValidator;
 
 /** Brinda el acceso a los validadores de formato o de líneas lógicas */
@@ -52,19 +44,12 @@ public class ValidatorManager {
       return logicalValidator;
     }
 
-    LogicalValidatorHandler tryCatchValidator = new TryDeclarationValidator();
-    LogicalValidatorHandler flowControlDeclarationValidator = new FlowControlDeclarationValidator();
     LogicalValidatorHandler typeDeclarationValidator = new TypeDeclarationValidator();
     LogicalValidatorHandler methodDeclarationValidator = new MethodDeclarationValidator();
-    LogicalValidatorHandler lambdaDeclarationValidator = new LambdaDeclarationValidator();
-    LogicalValidatorHandler ternaryDeclarationValidator = new TernaryDeclarationValidator();
 
-    tryCatchValidator.setNextValidator(flowControlDeclarationValidator);
-    flowControlDeclarationValidator.setNextValidator(typeDeclarationValidator);
     typeDeclarationValidator.setNextValidator(methodDeclarationValidator);
-    methodDeclarationValidator.setNextValidator(lambdaDeclarationValidator);
-    lambdaDeclarationValidator.setNextValidator(ternaryDeclarationValidator);
-    logicalValidator = tryCatchValidator;
+    methodDeclarationValidator.setNextValidator(typeDeclarationValidator);
+    logicalValidator = typeDeclarationValidator;
 
     return logicalValidator;
   }
